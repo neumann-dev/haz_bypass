4fd672934f83f4f2cd7c4c1347693f4078617fb8
neumann-dev <pascal.neumann@secshell.de>
2024-10-14 02:15:32 +0200

    extension only do sth on paywall articles

--- scripts/content.js
+++ scripts/content.js
@@ -1,31 +1,57 @@
-window.addEventListener('load', function() {
+window.addEventListener("load", function () {
+  const delay = (ms) => new Promise((res) => setTimeout(res, ms));
+  const init = async () => {
+    await delay(2000);
 
-const delay = ms => new Promise(res => setTimeout(res, ms));
+    const mainContent = document.querySelector("#contentMain");
+    if (!mainContent)
+      return;
 
+    const hazPlusIcon = mainContent.querySelector(".PaidIconstyled__PaidIcon-sc-19nr71-0");
+    if (!hazPlusIcon)
+      return;
 
-const yourFunction = async () => {
-  await delay(1000);
-  const fusion = document.getElementById('fusion-metadata')
-  const jsonString = fusion.innerHTML.split(';Fusion.')[4].replace('globalContent=', '')
-  const parentDiv = document.querySelector('.ArticleHeadstyled__ArticleTeaserContainer-sc-tdzyy5-3')
-  const firstTexBlock = document.querySelector('.Textstyled__Text-sc-1cqv9mi-0')
+    const fusion = document.querySelector("#fusion-metadata");
+    const jsonString = fusion.innerHTML
+      .split(";Fusion.")[4]
+      .replace("globalContent=", "");
+    const parentDiv = document.querySelector(
+      ".ArticleHeadstyled__ArticleTeaserContainer-sc-tdzyy5-3"
+    );
+    const firstTexBlock = document.querySelector(
+      ".Textstyled__Text-sc-1cqv9mi-0"
+    );
 
-  firstTexBlock.style.height = "auto"
+    firstTexBlock.style.height = "auto";
 
-  try {
-      const parsedObject = JSON.parse(jsonString)
-      console.log(parsedObject)
-      parsedObject.elements.forEach(element => {
-          if (element.type === "ad" || element.type === "moreItems") {
-              return
-          }
-          const p = document.createElement('p')
-          const textNode = document.createTextNode(element.text)
-          parentDiv.appendChild(document.createTextNode(element.text))
+    try {
+      const parsedObject = JSON.parse(jsonString);
+      console.log(parsedObject);
+      parsedObject.elements.forEach((element) => {
+        if (
+          element.type === "ad" ||
+          element.type === "moreItems" ||
+          element.type === "piano" ||
+          element.type === "newsletterAd"
+        ) {
+          return;
+        }
+        var textElement;
+        if (element.type === "text") {
+          textElement = document.createElement("p");
+        } else if (element.type === "header") {
+          textElement = document.createElement("h3");
+        }
+
+        if (textElement) {
+          const textNode = document.createTextNode(element.text);
+          textElement.appendChild(textNode);
+          parentDiv.appendChild(textElement);
+        }
       });
-  }catch (error) {
-      console.log(error)  
-  }
-}
-yourFunction()
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  init();
 });
